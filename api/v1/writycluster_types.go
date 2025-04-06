c8fdd34c677002bcaa6d41e3ed7181fcef00da12
alirezaarzehgar <alirezaarzehgar82@gmail.com>
2025-04-07 01:34:20 +0330

    init
    Signed-off-by: alirezaarzehgar <alirezaarzehgar82@gmail.com>

---
+++ api/v1/writycluster_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// WrityClusterSpec defines the desired state of WrityCluster
+type WrityClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of WrityCluster. Edit writycluster_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// WrityClusterStatus defines the observed state of WrityCluster
+type WrityClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// WrityCluster is the Schema for the writyclusters API
+type WrityCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WrityClusterSpec   `json:"spec,omitempty"`
+	Status WrityClusterStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// WrityClusterList contains a list of WrityCluster
+type WrityClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []WrityCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&WrityCluster{}, &WrityClusterList{})
+}
